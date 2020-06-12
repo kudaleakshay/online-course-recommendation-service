03039db9c4ea4bedd7198336880e78bbfecbeb08
Akshay Kudale <akshaybkudale@gmail.com>
2020-06-12 15:07:32 +0530

    Add- Mysql database connectivity Add- html form data passing Add- yaml file for database credentials Add- SQL select query for showing data in html

--- README.md
+++ README.md
@@ -7,8 +7,17 @@ pip3 install virtualenv
 # creates environment venv_web_project (you can give any name)  
 python -m venv venv_web_project
 
-# activate your env  
+# activate your env in windows  
 venv_web_project\Scripts\activate
+
+# activate your env in Linux/Mac
+Source [venv_web_project\Scripts\activate] #Path to env folder
 
 # install flask  
 pip3 install flask
+
+# install Mysql Database Connector
+pip3 install flask-mysqldb
+
+# instll PyYaml
+pip install pyyaml
--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+import yaml
+
 
 app = Flask(__name__)
 
-@app.route('/')
+# Configure db
+with open('db.yaml') as f:
+    
+    db = yaml.load(f, Loader=yaml.FullLoader)
+
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_database']
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET','POST'])
 def index():
-    render_template('index.html')
+    if request.method == 'POST':
+        # Fetch form Data
+        course_details = request.form
+        name = course_details['course']
+        print(name)
+        return redirect('/home')
+    return render_template('index.html')
 
 
+@app.route('/home')
+def Home():
+    cur = mysql.connection.cursor()
+    result_value = cur.execute("select * from datasets_udemy_courses limit 10")
+    if result_value > 0:
+        course_details = cur.fetchall()
+        cur.close()
+        return render_template('home.html', course_details = course_details)
+    
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
